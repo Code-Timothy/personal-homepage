bf2978d80034244b2cf3967b8fb2cf78f8427cb4
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-29 23:49:54 +0100

    add Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Container } from "./common/Container/inedx";
+import Header from "./features/Header";
 
 function App() {
   return (
     <Container>
+      <Header />
     </Container>
   );
-}
+};
 
 export default App;
---
+++ src/features/Header/index.js
@@ -0,0 +1,12 @@
+const Header = () => (
+    <header>
+        <img></img>
+        <div>
+            <span>This is</span>
+            <h1>Tymoteusz Wójcik</h1>
+            <p>🤵🏻💻 I`m passionate Fontend Developer in love with React, currently looking for new job opportunities.</p>
+        </div>
+    </header>
+);
+
+export default Header;
