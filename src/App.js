70e775dd8749b82f3942b2865045c0ece92f4879
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-01 23:53:07 +0100

    add Section component with test content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Container } from "./common/Container/inedx";
 import Header from "./features/Header";
+import Section from "./features/Section";
 
 function App() {
   return (
     <Container>
       <Header />
+      <Section />
     </Container>
   );
 };
---
+++ src/features/Section/index.js
@@ -0,0 +1,13 @@
+const Section = () => (
+    <section>
+        <header>My skillset includes 🛠</header>
+        <ul>
+            <li>Semantic & accessible HTML</li>
+            <li>Responsive Web Design</li>
+            <li>Teamwork</li>
+            <li>Markdown</li>
+        </ul>
+    </section>
+);
+
+export default Section;
