7ff37c9cf8afc826b23851b5989a1b2c2e1eff56
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-06 22:18:48 +0100

    add Footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Container } from "./common/Container/inedx";
 import Header from "./features/Header";
 import Section from "./features/Section";
+import Footer from "./features/Footer";
 import LearningGoalsList from "./features/Section/LearningGoalsList";
 import SkillsList from "./features/Section/SkillsList";
 
@@ -16,6 +17,7 @@ function App() {
         title="What I want to learn next 🚀"
         body={<LearningGoalsList />}
       />
+      <Footer />
     </Container>
   );
 };
---
+++ src/features/Footer/index.js
@@ -0,0 +1,13 @@
+import Content from "../Header/Content";
+
+const Footer = () => (
+    <div>
+        <Content
+            title="timmywojcik@gmail.com"
+            subtitle="Let's talk!"
+            description="I'm always open to new projects whenever I gave the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come to life, feel free to contact me ✌🏻"
+        />
+    </div>
+);
+
+export default Footer;
