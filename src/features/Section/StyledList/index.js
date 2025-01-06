9b8227388142917d1c7b1c5e6c081b09df7c72da
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-06 20:29:06 +0100

    style List component in StyledList

--- src/features/Section/StyledList/index.js
+++ src/features/Section/StyledList/index.js
@@ -3,9 +3,11 @@ import styled from "styled-components";
 export const List = styled.ul`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
-    gap: 10px;
+    grid-column-gap: 115px;
+    grid-row-gap: 10px;
     margin: 0;
-    padding: 32px 32px 0 32px;
+    padding: 32px 0 0 0;
+    list-style-position: inside;
 `;
 
 export const Item = styled.li`
