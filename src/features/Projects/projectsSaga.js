aa132e74c730d2a89a201c5597be7ec0b59ffd25
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-07 22:57:19 +0100

    add delay before fetching GitHub projects to display loading screen

--- src/features/Projects/projectsSaga.js
+++ src/features/Projects/projectsSaga.js
@@ -1,9 +1,10 @@
-import { put, call, takeLatest } from "redux-saga/effects";
+import { put, call, delay, takeLatest } from "redux-saga/effects";
 import { fetchProjectsFailure, fetchProjectsRequest, fetchProjectsSuccess } from "./projectsSlice";
 import { fetchGithubProjects } from "./fetchGithubProjects";
 
 function* fetchGithubProjectsHandler() {
     try {
+        yield delay(2500);
         const projects = yield call(fetchGithubProjects);
         yield put(fetchProjectsSuccess(projects));
     } catch (error) {
