5b3ae381ba007a93a56042e325fc6773f6b91663
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-07 23:35:07 +0100

    show appropriate views for loading and error states

--- src/features/Projects/index.js
+++ src/features/Projects/index.js
@@ -1,15 +1,27 @@
+import { useSelector } from "react-redux";
 import Tiles from "./Tiles";
+import Loading from "./Loading";
+import Error from "./Error";
 import { Wrapper, StyledHeader, StyledGithubIcon, Title, Subtitle } from "./styled";
+import { selectError, selectLoading, selectProjects } from "./projectsSlice";
 
-const Projects = () => (
-    <Wrapper>
-        <StyledHeader>
-            <StyledGithubIcon />
-            <Title>Portfolio</Title>
-            <Subtitle>My recent projects</Subtitle>
-        </StyledHeader>
-        <Tiles />
-    </Wrapper>
-);
+const Projects = () => {
+    const loading = useSelector(selectLoading);
+    const projects = useSelector(selectProjects);
+    const error = useSelector(selectError);
+
+    return (
+        <Wrapper>
+            <StyledHeader>
+                <StyledGithubIcon />
+                <Title>Portfolio</Title>
+                <Subtitle>My recent projects</Subtitle>
+            </StyledHeader>
+            {loading && <Loading />}
+            {projects && <Tiles />}
+            {error && <Error />}
+        </Wrapper>
+    );
+};
 
 export default Projects;
