002841f4b8b4b2b24573173594eaed3d91b9c5a8
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-07 22:18:43 +0100

    render projects list using map

--- src/features/Projects/Tiles/index.js
+++ src/features/Projects/Tiles/index.js
@@ -1,3 +1,6 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchProjectsRequest, selectProjects } from "../projectsSlice";
 import {
     Wrapper,
     StyledTile,
@@ -7,49 +10,31 @@ import {
     Link,
 } from "./styled";
 
-const Tiles = () => (
-    <Wrapper>
-        <StyledTile>
-            <Title>Movie Browser</Title>
-            <Description>Project description, e.g website where you can search for favourite movies and people. Project descritpion, e.g website where you can search</Description>
-            <LinksContainer>
-                <span>Demo:</span>
-                <span><Link href="x">https://link.demo.com</Link></span>
-                <span>Code:</span>
-                <span><Link href="x">https://link.demo.com</Link></span>
-            </LinksContainer>
-        </StyledTile>
-        <StyledTile>
-            <Title>Movie Browser</Title>
-            <Description>Project description, e.g website where you can search for favourite movies and people. Project descritpion, e.g website where you can search</Description>
-            <LinksContainer>
-                <span>Demo:</span>
-                <span><Link href="x">https://link.demo.com</Link></span>
-                <span>Code:</span>
-                <span><Link href="x">https://link.demo.com</Link></span>
-            </LinksContainer>
-        </StyledTile>
-        <StyledTile>
-            <Title>Movie Browser</Title>
-            <Description>Project description, e.g website where you can search for favourite movies and people. Project descritpion, e.g website where you can search</Description>
-            <LinksContainer>
-                <span>Demo:</span>
-                <span><Link href="x">https://link.demo.com</Link></span>
-                <span>Code:</span>
-                <span><Link href="x">https://link.demo.com</Link></span>
-            </LinksContainer>
-        </StyledTile>
-        <StyledTile>
-            <Title>Movie Browser</Title>
-            <Description>Project description, e.g website where you can search for favourite movies and people. Project descritpion, e.g website where you can search</Description>
-            <LinksContainer>
-                <span>Demo:</span>
-                <span><Link href="x">https://link.demo.com</Link></span>
-                <span>Code:</span>
-                <span><Link href="x">https://link.demo.com</Link></span>
-            </LinksContainer>
-        </StyledTile>
-    </Wrapper>
-);
+const Tiles = () => {
+    const projects = useSelector(selectProjects);
+    console.log(projects)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchProjectsRequest());
+    }, []);
+
+    return (
+        <Wrapper>
+            {projects.map(project => (
+                <StyledTile>
+                    <Title>{project.name}</Title>
+                    <Description>{project.description}</Description>
+                    <LinksContainer>
+                        <span>Demo:</span>
+                        <span><Link href={project.homepage} target="_blank" rel="noreferrer noopener">{project.homepage}</Link></span>
+                        <span>Code:</span>
+                        <span><Link href={project.svn_url} target="_blank" rel="noreferrer noopener">{project.svn_url}</Link></span>
+                    </LinksContainer>
+                </StyledTile>
+            ))}
+        </Wrapper>
+    );
+};
 
 export default Tiles;
