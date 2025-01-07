e2a953b5efb80639505e7d2c0c87b260bb921b85
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-07 23:32:23 +0100

    add color to theme

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,7 @@ export const theme = {
         white: "#FFFFFF",
         whiteLilac: "#FBFBFE",
         mineShaft: "#252525",
+        woodSmoke: "18181B",
         slateGray: "#6E7E91",
         scienceBlue: "#0366D6",
         anakiwa: "#8CC2FF",
