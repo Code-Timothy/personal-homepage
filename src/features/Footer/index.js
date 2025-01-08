cc6908639dfd399c12f9a625ee68fb5a30d0dada
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-08 22:11:05 +0100

    make footer title a link and add hover effect

--- src/features/Footer/index.js
+++ src/features/Footer/index.js
@@ -1,11 +1,11 @@
 import Content from "../Content";
 import SocialIcons from "./SocialIcons";
-import { Wrapper } from "./styled";
+import { Wrapper, Link } from "./styled";
 
 const Footer = () => (
     <Wrapper>
         <Content
-            title="timmywojcik@gmail.com"
+            title={<Link href="mailto:timmywojcik@gmail.com">timmywojcik@gmail.com</Link>}
             subtitle="Let's talk!"
             description="I'm always open to new projects whenever I gave the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come to life, feel free to contact me ✌🏻"
             additionalContent={<SocialIcons />}
--- src/features/Footer/styled.js
+++ src/features/Footer/styled.js
@@ -7,3 +7,13 @@ export const Wrapper = styled.div`
         margin: 48px 0;
     };
 `;
+
+export const Link = styled.a`
+    color: ${({ theme }) => theme.text.header};
+    text-decoration: none;
+    transition: color 0.5s;
+
+    &:hover{
+        color: ${({ theme }) => theme.tile.link};
+    };
+`;
