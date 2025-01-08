16c7778f0846276465d7c873f9908adb753e33c1
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-09 00:53:37 +0100

    move ThemeProvider and GlobalStyles to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useSelector } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './theme';
+import GlobalStyles from './globalStyles';
 import PersonalHomepage from "./features/PersonalHomepage";
+import { selectDarkTheme } from './features/PersonalHomepage/Header/ThemeButton/themeSlice';
 
 function App() {
+  const theme = useSelector(selectDarkTheme);
+
   return (
-    <PersonalHomepage />
+    <ThemeProvider theme={theme ? darkTheme : lightTheme}>
+      <GlobalStyles />
+      <PersonalHomepage />
+    </ThemeProvider>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
 import store from "./store";
-import { darkTheme, lightTheme } from './theme';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import GlobalStyles from './globalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={lightTheme}>
-        <GlobalStyles />
-        <App />
-      </ThemeProvider>
+      <App />
     </Provider>
   </React.StrictMode>
 );
