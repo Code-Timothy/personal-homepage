8fb680b76240654cdc101ff9553436ffca09a2db
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-09 00:27:32 +0100

    add themeReducer to store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { projectsReducer } from "./features/Projects/projectsSlice";
+import { themeReducer } from "./features/Header/ThemeButton/themeSlice";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./rootSaga";
 
@@ -8,6 +9,7 @@ const sagaMiddleware = createSagaMiddleware();
 const store = configureStore({
     reducer: {
         projects: projectsReducer,
+        theme: themeReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(sagaMiddleware),
