af34277cc26ca78342fb18aa90201f3c036731eb
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-11 00:26:34 +0100

    add new description to Footer

--- src/features/PersonalHomepage/Footer/index.js
+++ src/features/PersonalHomepage/Footer/index.js
@@ -7,7 +7,12 @@ const Footer = () => (
         <Content
             title={<Link href="mailto:timmywojcik@gmail.com">timmywojcik@gmail.com</Link>}
             subtitle="Let's talk!"
-            description="I'm always open to new projects whenever I gave the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come to life, feel free to contact me ✌🏻"
+            description="I'm always open to taking on exciting new projects whenever I have the time. 
+                        you have a website, dashboard, mobile app, or any other digital product in mind,
+                        I'here to help bring your ideas to life. Passionate about building intuitive,
+                        user-friendly experiences with clean, maintainable code,
+                        I'd love to collaborate on your next project. Feel free to get in touch - 
+                        I'm excited to help you turn your vision into reality! ✌🏻"
             additionalContent={<SocialIcons />}
         />
     </Wrapper>
