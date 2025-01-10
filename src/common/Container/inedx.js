363bbb650565d0187be4c4b3a0026a705eeb293f
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-10 20:46:01 +0100

    change margin and add media-queries

--- src/common/Container/inedx.js
+++ src/common/Container/inedx.js
@@ -2,7 +2,11 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     max-width: 1216px;
-    margin: auto;
+    margin: 62px auto 0 auto;
     color: ${({ theme }) => theme.text.header};
     padding: 16px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.tabletLandscape}px){
+        margin: auto;
+    };
 `;
