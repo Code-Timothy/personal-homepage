a1c05655bb47fddf91fbd536104ab6dc8d08e91f
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-11 14:08:26 +0100

    change description in footer and header

--- src/features/PersonalHomepage/Footer/index.js
+++ src/features/PersonalHomepage/Footer/index.js
@@ -7,12 +7,11 @@ const Footer = () => (
         <Content
             title={<Link href="mailto:timmywojcik@gmail.com">timmywojcik@gmail.com</Link>}
             subtitle="Let's talk!"
-            description="I'm always open to taking on exciting new projects whenever I have the time. 
-                        you have a website, dashboard, mobile app, or any other digital product in mind,
-                        I'here to help bring your ideas to life. Passionate about building intuitive,
-                        user-friendly experiences with clean, maintainable code,
-                        I'd love to collaborate on your next project. Feel free to get in touch - 
-                        I'm excited to help you turn your vision into reality! ✌🏻"
+            description="I'm always open to taking on exciting new projects whenever I have the time.
+                        If you have a website, dashboard, mobile app, or any other digital product in mind,
+                        I'm here to help bring your ideas to life. Passionate about building intuitive,
+                        user-friendly experiences with clean, maintainable code, I'd love to collaborate on your next project.
+                        Feel free to get in touch - I'm excited to help turn your vision into reality! ✌🏻"
             additionalContent={<SocialIcons />}
         />
     </Wrapper>
--- src/features/PersonalHomepage/Header/AboutMe/index.js
+++ src/features/PersonalHomepage/Header/AboutMe/index.js
@@ -1,8 +1,8 @@
 import { Text } from "./styled";
 
 const AboutMe = () => (
-    <Text>I am a young and passionate <strong>Frontend (React) developer</strong> in love with React ⚛️,
-        with a keen interest in exploring and mastering new technologies. I prioritize creating clean,
+    <Text>I am a young and passionate <strong>Frontend (React) developer</strong> who is deeply in love with React ⚛️,
+        with a strong interest in exploring and mastering new technologies. I prioritize writing clean,
         maintainable code and user-friendly UX/UI design to ensure the best possible experience for users.
         Currently, I am <strong>looking for exciting new job opportunities</strong> to further enhance
         my skills and contribute to innovative projects 🚀.
